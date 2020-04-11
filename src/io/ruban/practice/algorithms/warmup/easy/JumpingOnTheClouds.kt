786a437be0f4be32cb8276c9ad6cb9153e2fd9f2
andreiruban <herr.ruban@yandex.com>
2020-04-11 15:01:55 +0200

    JumpingOnTheClouds.kt quiz has been added.

---
+++ src/io/ruban/practice/algorithms/warmup/easy/JumpingOnTheClouds.kt
@@ -0,0 +1,25 @@
+package io.ruban.practice.algorithms.warmup.easy
+
+fun jumpingOnClouds(c: Array<Int>): Int {
+    var steps: Int = 0
+    var nextId: Int = 0
+
+    outer@ while (nextId < c.size - 1) {
+        inner@ for (inc in 2 downTo 1) {
+            if (nextId + inc < c.size && c[nextId + inc] == 0) {
+                steps += 1
+                nextId += inc
+                break@inner
+            }
+        }
+    }
+    return steps
+}
+
+fun main(args: Array<String>) {
+    val arr1 = intArrayOf(0, 0, 0, 1, 0, 0).toTypedArray()
+    val arr2 = intArrayOf(0, 0, 1, 0, 0, 1, 0).toTypedArray()
+
+    println(jumpingOnClouds(arr1))
+    println(jumpingOnClouds(arr2))
+}
