2880808f124e77d21a108f1563615c5ef1f6a736
andreiruban <herr.ruban@yandex.com>
2020-04-12 00:25:15 +0200

    CountingValleys.kt quiz has been added.

---
+++ src/io/ruban/practice/algorithms/warmup/easy/CountingValleys.kt
@@ -0,0 +1,29 @@
+package io.ruban.practice.algorithms.warmup.easy
+
+// Complete the countingValleys function below.
+fun countingValleys(n: Int, s: String): Int {
+    var seaLevel: Int = 0
+    var valleyCount: Int = 0
+    var isValley: Boolean = false
+
+    s.forEach { step ->
+        if (step == 'D') {
+            if (seaLevel == 0) isValley = true
+            seaLevel--
+        } else seaLevel++
+        if (isValley && seaLevel == 0) {
+            valleyCount++
+            isValley = false
+        }
+    }
+
+    return valleyCount
+}
+
+fun main() {
+    val pair1: Pair<Int, String> = Pair(8, "UDDDUDUU")
+
+    val result = countingValleys(n = pair1.first, s = pair1.second)
+
+    println(result)
+}
